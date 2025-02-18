ac3bc91eaf943b51697b2f63bd0acc6d2ab2b1cc
juan guillermo <juang961225@gmail.com>
2025-02-18 15:47:42 -0500

    add funtions tables

--- pages/list.vue
+++ pages/list.vue
@@ -4,7 +4,6 @@ interface FormSubmission {
   id: string;
   contents?: string;
   course?: string;
-  createdAt?: string;
   dba?: string;
   description?: string;
   indicator?: string;
@@ -15,7 +14,6 @@ interface FormSubmission {
   score?: number;
   subject?: string;
   targetPeriod?: string;
-  toggle?: boolean;
 }
 
 interface Column {
@@ -23,6 +21,24 @@ interface Column {
   label?: string;
 }
 
+const columns: Column[] = [
+  { key: 'id', sortable: true, label: 'ID' },
+  { key: 'contents', sortable: true, label: 'Contents' },
+  { key: 'course', sortable: true, label: 'Course' },
+  { key: 'dba', sortable: true, label: 'DBA' },
+  { key: 'description', sortable: true, label: 'Description' },
+  { key: 'indicator', sortable: true, label: 'Indicator' },
+  { key: 'materialLink', sortable: true, label: 'Material Link' },
+  { key: 'materialName', sortable: true, label: 'Material Name' },
+  { key: 'materialType', sortable: true, label: 'Material Type' },
+  { key: 'period', sortable: true, label: 'Period' },
+  { key: 'score', sortable: true, label: 'Score' },
+  { key: 'subject', sortable: true, label: 'Subject' },
+  { key: 'targetPeriod', sortable: true, label: 'Target Period' },
+  { key: 'actions' }
+];
+
+const selectedColumns = ref([...columns])
 const { getFormSubmissions } = useFirebaseDB();
 const formSubmissions: Ref<FormSubmission[]> = ref([]);
 
@@ -36,7 +52,7 @@ const fetchData = async (): Promise<void> => {
 };
 fetchData();
 
-const people = computed<FormSubmission[]>(() => 
+const people = computed<FormSubmission[]>(() =>
   formSubmissions.value.map(sub => ({
     id: sub.id,
     contents: sub.contents ?? '',
@@ -51,32 +67,12 @@ const people = computed<FormSubmission[]>(() =>
     score: sub.score ?? 0,
     subject: sub.subject ?? '',
     targetPeriod: sub.targetPeriod ?? '',
-    toggle: sub.toggle ?? false,
   }))
 );
 
 watch(people, (newPeople) => {
   console.log('Datos transformados:', newPeople);
 });
-
-const columns: Column[] = [
-  { key: 'id', label: 'ID' },
-  { key: 'contents', label: 'Contents' },
-  { key: 'course', label: 'Course' },
-  { key: 'dba', label: 'DBA' },
-  { key: 'description', label: 'Description' },
-  { key: 'indicator', label: 'Indicator' },
-  { key: 'materialLink', label: 'Material Link' },
-  { key: 'materialName', label: 'Material Name' },
-  { key: 'materialType', label: 'Material Type' },
-  { key: 'period', label: 'Period' },
-  { key: 'score', label: 'Score' },
-  { key: 'subject', label: 'Subject' },
-  { key: 'targetPeriod', label: 'Target Period' },
-  { key: 'toggle', label: 'Toggle' },
-  { key: 'actions' }
-];
-
 
 const items = (row: FormSubmission) => [
   [{ label: 'Edit', icon: 'i-heroicons-pencil-square-20-solid', click: () => console.log('Edit', row.id) }],
@@ -88,13 +84,20 @@ const selected: Ref<FormSubmission[]> = ref([]);
 </script>
 
 <template>
-  <UTable v-model="selected" :rows="people" :columns="columns">
+  <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+    <USelectMenu v-model="selectedColumns" :options="columns" multiple placeholder="Columns" />
+  </div>
+
+  <UTable :columns="selectedColumns" v-model="selected" :rows="people">
     <template #materialName-data="{ row }">
-      <span :class="[selected.find(p => p.id === row.id) && 'text-primary-500 dark:text-primary-400']">{{ row.materialName }}</span>
+      <span :class="[selected.find(p => p.id === row.id) && 'text-primary-500 dark:text-primary-400']">{{
+        row.materialName }}</span>
     </template>
 
     <template #actions-data="{ row }">
-      <UDropdown :items="items(row)"><UButton color="gray" variant="ghost" icon="i-heroicons-ellipsis-horizontal-20-solid" /></UDropdown>
+      <UDropdown :items="items(row)">
+        <UButton color="gray" variant="ghost" icon="i-heroicons-ellipsis-horizontal-20-solid" />
+      </UDropdown>
     </template>
   </UTable>
 </template>
